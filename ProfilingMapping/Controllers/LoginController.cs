a1ca59f334eca6d5d7e0aada4e845a8606e466cf
Jomiel <enriquez.jliquigan@gmail.com>
2023-05-16 11:08:01 +0800

    Login Function

--- ProfilingMapping/Controllers/LoginController.cs
+++ ProfilingMapping/Controllers/LoginController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ProfilingMapping.Models;
+using ProfilingMapping.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +10,29 @@ namespace ProfilingMapping.Controllers
 {
     public class LoginController : Controller
     {
+        public static string errorMessage = string.Empty;
         public ActionResult Login()
         {
+            ViewBag.ErrorMessage = errorMessage;
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Log_in(SystemModel systemModel)
+        {
+            Guid adminid = LoginRepository.HasAccess(systemModel.username, systemModel.password);
+
+            if(adminid == Guid.Empty)
+            {
+                errorMessage = "Invalid Username or Password.. Please log in again.";
+                return RedirectToAction("Login");
+            }
+            else
+            {
+                LogiInModel.adminID = adminid;
+                return RedirectToAction("../Home/Index");
+            }
+
         }
     }
 }
